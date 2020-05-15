c1e59c2f8b4d6ceec6a6ff54916e770dff138287
TomerSitton <tomer.sitton1@gmail.com>
2020-05-15 15:24:34 +0300

    made storage not static

--- tomers_tests/storage.py
+++ tomers_tests/storage.py
@@ -18,40 +18,35 @@
 ########################################################################
 
 import sqlite3
+import os
+
+
+class ItemOps:
+    Insert = 0
+    Delete = 1
+    Update = 2
+    SoftDelete = 3
+
 
 class storage:
-    @staticmethod
-    def set_keyvalue(key, value, path="pyas.asdb"):
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        curs.execute("INSERT INTO KeyValue VALUES ('%s', '%s')" % (key, value))
-        conn.commit()
-        conn.close()
-    
-    @staticmethod
-    def update_keyvalue(key, value, path="pyas.asdb"):
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        sql = "UPDATE KeyValue SET Value='%s' WHERE Key='%s'" % (value.replace("'","''"), key)
-        curs.execute(sql)
-        conn.commit()
-        conn.close()
 
-    @staticmethod
-    def get_keyvalue(key, path="pyas.asdb"):
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        curs.execute("SELECT Value FROM KeyValue WHERE Key='%s'" % key)
-        try:
-            value = curs.fetchone()[0]
-            conn.close()
-            return value
-        except:
-            conn.close()
-            return None
+    def __init__(self, path="pyas.asdb", force_create=False):
+        """
+        This function creates a new storage instance for the DB specified in the path variable.
+        If the DB in the path variable does not exist - it creates it.
+        With the force_create - create the db in path even if there is already a file there.
+        :param path: The path of the DB
+        :type path: str
+        :param force_create: force the creation of a new db, even if there is already one.
+        :type force_create: bool
+        """
+        self.conn = sqlite3.connect(path)
+        self.curs = self.conn.cursor()
 
-    @staticmethod
-    def create_db(path=None):
+        if force_create or not os.path.isfile(path):
+            self.create_db(path)
+
+    def create_db(self, path):
         """
         Create a DB with the following tables:
 
@@ -99,19 +94,12 @@ class storage:
         :param path: The path of the DB
         :return: None
         """
-        if path:
-            if path != "pyas.asdb":
-                if not path[-1] == "\\":
-                    path = path + "\\pyas.asdb"
-        else:
-            path="pyas.asdb"
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        curs.execute("""CREATE TABLE FolderHierarchy (ServerId text, ParentId text, DisplayName text, Type text)""")
-        curs.execute("""CREATE TABLE SyncKeys (SyncKey text, CollectionId text)""")
-        curs.execute("""CREATE TABLE KeyValue (Key text, Value blob)""")
+        self.curs.execute(
+            """CREATE TABLE FolderHierarchy (ServerId text, ParentId text, DisplayName text, Type text)""")
+        self.curs.execute("""CREATE TABLE SyncKeys (SyncKey text, CollectionId text)""")
+        self.curs.execute("""CREATE TABLE KeyValue (Key text, Value blob)""")
 
-        curs.execute("""CREATE TABLE MSASEMAIL (ServerId text, 
+        self.curs.execute("""CREATE TABLE MSASEMAIL (ServerId text, 
                                                 email_To text, 
                                                 email_Cc text,
                                                 email_From text,
@@ -141,61 +129,59 @@ class storage:
                                                 airsyncbase_BodyPart text,
                                                 email2_AccountId text,
                                                 rm_RightsManagementLicense text)""")
-        
-        conn.commit()
 
-        indicies = ['CREATE UNIQUE INDEX "main"."MSASEMAIL_ServerId_Idx" ON "MSASEMAIL" ("ServerId" ASC)', 
+        self.conn.commit()
+
+        indicies = ['CREATE UNIQUE INDEX "main"."MSASEMAIL_ServerId_Idx" ON "MSASEMAIL" ("ServerId" ASC)',
                     'CREATE UNIQUE INDEX "main"."SyncKey_CollectionId_Idx" ON "SyncKeys" ("CollectionId" ASC)',
                     'CREATE UNIQUE INDEX "main"."KeyValue_Key_Idx" ON "KeyValue" ("Key" ASC)',
                     'CREATE UNIQUE INDEX "main"."FolderHierarchy_ServerId_Idx" ON "FolderHierarchy" ("ServerId" ASC)',
                     'CREATE  INDEX "main"."FolderHierarchy_ParentType_Idx" ON "FolderHierarchy" ("ParentId" ASC, "Type" ASC)',
                     ]
         for index in indicies:
-            curs.execute(index)
-        #storage.set_keyvalue("X-MS-PolicyKey", "0")   --- this is used for policy. part of the Provision command. i think it is not necessary
-        #storage.set_keyvalue("EASPolicies", "")        --- this is used for policy. part of the Provision command. i think it is not necessary
-        #storage.set_keyvalue("MID", "0")               --- mail id...? i think its used just for sending mails
-        conn.commit()
+            self.curs.execute(index)
+        # storage.set_keyvalue("X-MS-PolicyKey", "0")   --- this is used for policy. part of the Provision command. i think it is not necessary
+        # storage.set_keyvalue("EASPolicies", "")        --- this is used for policy. part of the Provision command. i think it is not necessary
+        # storage.set_keyvalue("MID", "0")               --- mail id...? i think its used just for sending mails
+        self.conn.commit()
 
-        conn.close()
-    
-    @staticmethod
-    def get_conn_curs(path="pyas.asdb"):
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        return conn, curs
 
-    @staticmethod
-    def close_conn_curs(conn):
+    def set_keyvalue(self, key, value):
+        self.curs.execute("INSERT INTO KeyValue VALUES ('%s', '%s')" % (key, value))
+        self.conn.commit()
+
+    def update_keyvalue(self, key, value):
+        sql = "UPDATE KeyValue SET Value='%s' WHERE Key='%s'" % (value.replace("'","''"), key)
+        self.curs.execute(sql)
+        self.conn.commit()
+
+    def get_keyvalue(self, key, path="pyas.asdb"):
+        self.curs.execute("SELECT Value FROM KeyValue WHERE Key='%s'" % key)
         try:
-            conn.commit()
-            conn.close()
+            value = self.curs.fetchone()[0]
+            return value
         except:
-            return False
-        return True
+            return None
 
 
-    @staticmethod
-    def insert_folderhierarchy_change(folder, curs):
+    def insert_folderhierarchy_change(self, folder):
         sql = """INSERT INTO FolderHierarchy VALUES ("%s", "%s", "%s", "%s")""" % (folder.ServerId, folder.ParentId, folder.DisplayName, folder.Type)
-        curs.execute(sql)
+        self.curs.execute(sql)
 
-    @staticmethod
-    def update_folderhierarchy_change(folder, curs):
+    def update_folderhierarchy_change(self, folder):
         sql = "UPDATE FolderHierarchy SET ParentId='%s', DisplayName='%s', Type='%s' WHERE ServerId == '%s'""" % (folder.ParentId, folder.DisplayName, folder.Type, folder.ServerId)
-        curs.execute(sql)
+        self.curs.execute(sql)
 
-    @staticmethod
-    def delete_folderhierarchy_change(folder, curs):
+
+    def delete_folderhierarchy_change(self, folder):
         #Command only sent we permement delete is requested. Otherwise it would be 'Update' to ParentId='3' (Deleted Items).
         #sql = "UPDATE FolderHierarchy SET ParentId='4' WHERE ServerId == '%s'""" % (folder.ServerId)
         sql = "DELETE FROM MSASEMAIL WHERE ServerId like '%s:%%'" % (folder.ServerId)
-        curs.execute(sql)
+        self.curs.execute(sql)
         sql = "DELETE FROM FolderHierarchy WHERE ServerId == '%s'" % (folder.ServerId)
-        curs.execute(sql)
+        self.curs.execute(sql)
 
-    @staticmethod
-    def update_folderhierarchy(changes, path="pyas.asdb"):
+    def update_folderhierarchy(self, changes):
         """
         :param changes: list of tuples of command and objects.
                         for example:
@@ -204,40 +190,34 @@ class storage:
         :param path:
         :return:
         """
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
         for change in changes:
             if change[0] == "Update":
-                storage.update_folderhierarchy_change(change[1], curs)
+                self.update_folderhierarchy_change(change[1])
             elif change[0] == "Delete":
-                storage.delete_folderhierarchy_change(change[1], curs)
+                self.delete_folderhierarchy_change(change[1])
             elif change[0] == "Add":
-                storage.insert_folderhierarchy_change(change[1], curs)
-        conn.commit()
-        conn.close()
+                self.insert_folderhierarchy_change(change[1])
+        self.conn.commit()
 
-    @staticmethod
-    def get_folderhierarchy_folder_by_name(foldername, curs):
+    def get_folderhierarchy_folder_by_name(self, foldername):
         sql = "SELECT * FROM FolderHierarchy WHERE DisplayName = '%s'" % foldername
-        curs.execute(sql)
-        folder_row = curs.fetchone()
+        self.curs.execute(sql)
+        folder_row = self.curs.fetchone()
         if folder_row:
             return folder_row
         else:
             return False
 
-    @staticmethod
-    def get_folderhierarchy_folder_by_id(server_id, curs):
+    def get_folderhierarchy_folder_by_id(self, server_id):
         sql = "SELECT * FROM FolderHierarchy WHERE ServerId = '%s'" % server_id
-        curs.execute(sql)
-        folder_row = curs.fetchone()
+        self.curs.execute(sql)
+        folder_row = self.curs.fetchone()
         if folder_row:
             return folder_row
         else:
             return False
 
-    @staticmethod
-    def insert_item(table, calendar_dict, curs):
+    def insert_item(self, table, calendar_dict):
         server_id = calendar_dict["server_id"]
         del calendar_dict["server_id"]
         calendar_cols = ""
@@ -246,10 +226,9 @@ class storage:
             calendar_cols += (", '%s'" % calendar_field)
             calendar_vals += (", '%s'"  % repr(calendar_dict[calendar_field]).replace("'","''"))
         sql = "INSERT INTO %s ( 'ServerId' %s ) VALUES ( '%s' %s )" % (table, calendar_cols, server_id, calendar_vals)
-        curs.execute(sql)
+        self.curs.execute(sql)
 
-    @staticmethod
-    def update_item(table, calendar_dict, curs):
+    def update_item(self, table, calendar_dict):
         server_id = calendar_dict["server_id"]
         del calendar_dict["server_id"]
         calendar_sql = ""
@@ -257,133 +236,85 @@ class storage:
             calendar_sql += (", %s='%s' "  % (calendar_field, repr(calendar_dict[calendar_field]).replace("'","''")))
         calendar_sql = calendar_sql.lstrip(", ")
         sql = "UPDATE %s SET %s WHERE ServerId='%s'" % (table, calendar_sql, server_id)
-        curs.execute(sql)
-    
-    @staticmethod
-    def delete_item(table, sever_id, curs):
+        self.curs.execute(sql)
+
+    def delete_item(self, table, sever_id):
         sql = "DELETE FROM %s WHERE ServerId='%s'" % (table, sever_id)
-        curs.execute(sql)
+        self.curs.execute(sql)
 
-    class ItemOps:
-        Insert = 0
-        Delete = 1
-        Update = 2
-        SoftDelete = 3
+    def item_operation(self, method, item_class, data):
+        class_to_table_dict = {
+            "Email": "MSASEMAIL"
+        }
+        if method == ItemOps.Insert:
+            self.insert_item(class_to_table_dict[item_class], data)
+        elif method == ItemOps.Delete:
+            self.delete_item(class_to_table_dict[item_class], data)
+        elif method == ItemOps.Update:
+            self.update_item(class_to_table_dict[item_class], data)
+        elif method == ItemOps.SoftDelete:
+            self.delete_item(class_to_table_dict[item_class], data)
 
-    class_to_table_dict = {
-                        "Email" : "MSASEMAIL",
-                        "Calendar" : "MSASCAL",
-                        "Contacts" : "MSASCNTC",
-                        "Tasks" : "MSASTASK",
-                        "Notes" : "MSASNOTE",
-                        "SMS" : "MSASMS",
-                        "Document" : "MSASDOC"
-                        }
-
-    @staticmethod
-    def item_operation(method, item_class, data, curs):
-        if method == storage.ItemOps.Insert:
-            storage.insert_item(storage.class_to_table_dict[item_class], data, curs)
-        elif method == storage.ItemOps.Delete:
-            storage.delete_item(storage.class_to_table_dict[item_class], data, curs)
-        elif method == storage.ItemOps.Update:
-            storage.update_item(storage.class_to_table_dict[item_class], data, curs)
-        elif method == storage.ItemOps.SoftDelete:
-            storage.delete_item(storage.class_to_table_dict[item_class], data, curs)
-
-    @staticmethod
-    def update_items(collections, path="pyas.asdb"):
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
+    def update_items(self, collections):
         for collection in collections:
             for command in collection.Commands:
                 if command[0] == "Add":
-                    storage.item_operation(storage.ItemOps.Insert, command[1][1], command[1][0], curs)
+                    storage.item_operation(storage.ItemOps.Insert, command[1][1], command[1][0])
                 if command[0] == "Delete":
-                    storage.item_operation(storage.ItemOps.Delete, command[1][1], command[1][0], curs)
+                    storage.item_operation(storage.ItemOps.Delete, command[1][1], command[1][0])
                 elif command[0] == "Change":
-                    storage.item_operation(storage.ItemOps.Update, command[1][1], command[1][0], curs)
+                    storage.item_operation(storage.ItemOps.Update, command[1][1], command[1][0])
                 elif command[0] == "SoftDelete":
-                    storage.item_operation(storage.ItemOps.SoftDelete, command[1][1], command[1][0], curs)
+                    storage.item_operation(storage.ItemOps.SoftDelete, command[1][1], command[1][0])
             if collection.SyncKey > 1:
-                storage.update_synckey(collection.SyncKey, collection.CollectionId, curs)
-                conn.commit()
+                storage.update_synckey(collection.SyncKey, collection.CollectionId)
+                self.conn.commit()
             else:
-                conn.close()
                 raise AttributeError("SyncKey incorrect")
 
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
-    @staticmethod
-    def get_emails_by_collectionid(collectionid, curs):
+    def get_emails_by_collectionid(self, collectionid):
         sql = "SELECT * from MSASEMAIL WHERE ServerId like '%s:%%'" % collectionid
-        curs.execute(sql)
-        return curs.fetchall()
+        self.curs.execute(sql)
+        return self.curs.fetchall()
 
-    @staticmethod
-    def update_synckey(synckey, collectionid, curs=None):
-        cleanup = False
-        if not curs:
-            cleanup = True
-            conn = sqlite3.connect("pyas.asdb")
-            curs = conn.cursor()
-        curs.execute("SELECT SyncKey FROM SyncKeys WHERE CollectionId = %s" % collectionid)
-        prev_synckey = curs.fetchone()
+    def update_synckey(self, synckey, collectionid):
+        self.curs.execute("SELECT SyncKey FROM SyncKeys WHERE CollectionId = %s" % collectionid)
+        prev_synckey = self.curs.fetchone()
         if not prev_synckey:
-            curs.execute("INSERT INTO SyncKeys VALUES ('%s', '%s')" % (synckey, collectionid))
+            self.curs.execute("INSERT INTO SyncKeys VALUES ('%s', '%s')" % (synckey, collectionid))
         else:
-            curs.execute("UPDATE SyncKeys SET SyncKey='%s' WHERE CollectionId='%s'" % (synckey, collectionid)) 
-        if cleanup:
-            conn.commit()
-            conn.close()
+            self.curs.execute("UPDATE SyncKeys SET SyncKey='%s' WHERE CollectionId='%s'" % (synckey, collectionid))
 
-    @staticmethod
-    def get_synckey(collectionid, path="pyas.asdb"):
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        curs.execute("SELECT SyncKey FROM SyncKeys WHERE CollectionId = %s" % collectionid)
+    def get_synckey(self, collectionid):
+        self.curs.execute("SELECT SyncKey FROM SyncKeys WHERE CollectionId = %s" % collectionid)
         try:
-            synckey = curs.fetchone()[0]
+            synckey = self.curs.fetchone()[0]
         except TypeError:
             synckey = "0"
-        conn.close()
         return synckey
 
-    @staticmethod
-    def create_db_if_none(path="pyas.asdb"):
-        import os
-        if not os.path.isfile(path):
-            storage.create_db(path)
-
-    @staticmethod
-    def get_folder_name_to_id_dict(path="pyas.asdb"):
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        curs.execute("SELECT DisplayName, ServerId FROM FolderHierarchy")
-        id_name_list_of_tuples = curs.fetchall()
+    def get_folder_name_to_id_dict(self):
+        self.curs.execute("SELECT DisplayName, ServerId FROM FolderHierarchy")
+        id_name_list_of_tuples = self.curs.fetchall()
         name_id_dict = {}
         for id_name in id_name_list_of_tuples:
             name_id_dict.update({ id_name[0] : id_name[1] })
-        conn.close()
         return name_id_dict
 
     @staticmethod
-    def get_synckeys_dict(curs, path="pyas.asdb"):
+    def get_synckeys_dict(self):
         """
         This function returns a dictionary which its keys are the collection_ids and its
         values are the collections' synckey.
         for example:
         {'0': '1', '9': '696602903', '15': '1148150257', '2': '1893202198', '5': '1845584800', '12': '980326785'}
-        :param curs: the storage curs
-        :param path: the db path
         :return: dict of collection_id to sync_key
         :rtype: dict of str to str
         """
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        curs.execute("SELECT * FROM SyncKeys")
-        synckeys_rows = curs.fetchall()
+        self.curs.execute("SELECT * FROM SyncKeys")
+        synckeys_rows = self.curs.fetchall()
         synckeys_dict = {}
         if synckeys_rows:
             if len(synckeys_rows) > 0:
@@ -391,20 +322,10 @@ class storage:
                     synckeys_dict.update({synckey_row[1]:synckey_row[0]})
         return synckeys_dict
 
-    @staticmethod
-    def get_new_mid(path="pyas.asdb"):
-        pmid = int(storage.get_keyvalue("MID"))
-        mid = str(pmid+1)
-        storage.update_keyvalue("MID", mid)
-        return mid
 
-    @staticmethod
-    def get_serverid_to_type_dict(path="pyas.asdb"):
-        conn = sqlite3.connect(path)
-        curs = conn.cursor()
-        curs.execute("SELECT * FROM FolderHierarchy")
-        folders_rows = curs.fetchall()
-        conn.close()
+    def get_serverid_to_type_dict(self):
+        self.curs.execute("SELECT * FROM FolderHierarchy")
+        folders_rows = self.curs.fetchall()
         folders_dict = {}
         if folders_rows:
             if len(folders_rows) > 0:
@@ -413,3 +334,20 @@ class storage:
         else:
             raise LookupError("No folders found in FolderHierarchy table. Did you run a FolderSync yet?")
         return folders_dict
+
+
+
+#   @staticmethod
+#    def get_new_mid(path="pyas.asdb"):
+#        pmid = int(storage.get_keyvalue("MID"))
+#        mid = str(pmid+1)
+#        storage.update_keyvalue("MID", mid)
+#        return mid
+
+#    @staticmethod
+#   def close_conn_curs(conn):
+#      try:
+#         conn.close()
+#    except:
+#       return False
+#  return True
